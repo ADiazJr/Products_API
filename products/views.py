f7fc21b15fe1f2ce030c6de7e06899ff17abd0ac
unknown <arturodiazmay@gmail.com>
2022-09-23 17:07:27 -0500

    Created New View Function + Added URL Pathway on Products App urls + Added an admin User

--- products/views.py
+++ products/views.py
@@ -1,7 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def products_list(request):
-    
-    return Response('ok')
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def products_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer =ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
