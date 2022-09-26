cda7c5f879ffa0c08789fa09ae6e58395a1cf0fe
unknown <arturodiazmay@gmail.com>
2022-09-26 11:45:43 -0500

    Added Post Functionality + Updated Settings to change Price Values into Floats

--- products/views.py
+++ products/views.py
@@ -4,13 +4,20 @@ from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
-@api_view(["GET"])
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(["GET", ])
 def products_detail(request, pk):
     try:
         product = Product.objects.get(pk=pk)
